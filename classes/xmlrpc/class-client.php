ab33ebddc01bf8a9e91e07b53bb9cac09b359ed5
Joakim Stenberg <stenberg.me@gmail.com>
2015-05-27 12:51:35 +0200

    Retry XML-RPC on specific error messages

--- classes/xmlrpc/class-client.php
+++ classes/xmlrpc/class-client.php
@@ -7,9 +7,14 @@ use \WP_HTTP_IXR_Client;
 class Client extends WP_HTTP_IXR_Client {
 
 	private $secret_key;
+	private $request_args;
 	private $filtered_request;
 	private $filtered_response;
+	private $attempts;
 
+	/**
+	 * Constructor.
+	 */
 	public function __construct() {
 
 		$endpoint   = 'http://[YOUR_ENDPOINT_HERE]';
@@ -34,11 +39,24 @@ class Client extends WP_HTTP_IXR_Client {
 
 		$this->secret_key = $secret_key;
 
+		// Arguments to send with the XML-RPC request.
+		$this->request_args = array();
+
+		// Number of request attempts.
+		$this->attempts = 0;
+
 		parent::__construct( trailingslashit( $endpoint ) . 'xmlrpc.php', false, false, CONTENT_STAGING_TRANSFER_TIMEOUT );
 	}
 
+	/************************************************************************
+	 * Perform Request.
+	 *
+	 * Operations carried out on Content Stage to handle sending a XML-RPC
+	 * request to Production.
+	 ************************************************************************/
+
 	/**
-	 * Perform the XML-RPC request and return the response.
+	 * Prepare and perform the XML-RPC request and return the response.
 	 *
 	 * @param string $method
 	 * @param array  $data
@@ -47,47 +65,213 @@ class Client extends WP_HTTP_IXR_Client {
 	 */
 	public function request( $method, $data = array() ) {
 
-		$args = $this->prepare_request_args( $method, $data );
-
-		// Allow custom path to send XML-RPC request to.
-		$this->path = apply_filters( 'sme_xmlrpc_path', $this->path );
-
-		// Allow custom headers.
-		$this->headers = apply_filters( 'sme_client_headers', array() );
+		$this->request_args = $this->prepare_request_args( $method, $data );
+		$this->path         = $this->prepare_request_path( $this->path );
+		$this->headers      = $this->prepare_request_headers( $this->headers );
 
 		// Send request.
-		$query_successful = $this->send( $args );
+		$query_successful = $this->send();
 
 		if ( ! $query_successful ) {
-
-			// Handle failed request.
-			return $this->filtered_response = $this->query_failed();
+			return $this->get_error_message();
 		}
 
 		// Get the XML-RPC response data.
-		return $this->filtered_response = unserialize( $this->decode( $this->getResponse() ) );
+		return unserialize( $this->decode( $this->filtered_response ) );
 	}
 
 	/**
 	 * Perform XML-RPC request.
 	 *
-	 * @param array $args
-	 *
 	 * @return bool
 	 */
-	public function send( $args ) {
+	private function send() {
 
 		// Disable SSL verification (based on user settings).
 		$this->disable_ssl_verification();
 
 		// Perform XML-RPC request. Returns true on success, false on failure.
-		$query_successful = call_user_func_array( array( $this, 'query' ), $args );
+		$query_successful = call_user_func_array( array( $this, 'query' ), $this->request_args );
 
 		// Enable SSL verification.
 		$this->enable_ssl_verification();
 
+		// On failure, increment number of request and retry.
+		if ( ! $query_successful ) {
+			$this->attempts++;
+			return $this->retry();
+		}
+
+		// Reset number of request attempts.
+		$this->attempts = 0;
+
+		// Get response.
+		$this->filtered_response = $this->getResponse();
+
 		return $query_successful;
 	}
+
+	/**
+	 * Retry sending the XML-RPC request.
+	 *
+	 * @return bool True on successful request, false on failure.
+	 */
+	private function retry() {
+
+		// Error message.
+		$msg = $this->getErrorMessage();
+
+		// Error messages that should trigger request to be re-sent.
+		$retry_triggers = array(
+			'transport error - HTTP status code was not 200 (500)'
+		);
+
+		// Anonymous function checking if a phrase from an error message is
+		// part of the actual error message.
+		$retry_callback = function( $carry, $item ) use ( $msg ) {
+			if ( $carry ) return $carry;
+			return strpos( $msg, $item ) !== false;
+		};
+
+		// Check if request should be re-sent.
+		$should_retry = array_reduce( $retry_triggers, $retry_callback, false );
+
+		// Request should not be re-sent, return error message.
+		if ( $this->attempts >= 3 || ! $should_retry ) {
+			$this->attempts = 0;
+			return false;
+		}
+
+		// Wait before trying to send the request again.
+		sleep( 5 * $this->attempts );
+
+		return $this->send();
+	}
+
+	/**
+	 * Arguments to be sent with the XML-RPC request.
+	 *
+	 * @param string $method
+	 * @param array  $data
+	 *
+	 * @return array
+	 */
+	private function prepare_request_args( $method, $data = array() ) {
+
+		$data = $this->encode( serialize( $data ) );
+
+		return array(
+			$method,
+			$this->generate_access_token( $data ),
+			$data,
+		);
+	}
+
+	/**
+	 * Set custom path to send XML-RPC request to.
+	 *
+	 * @param string $path
+	 *
+	 * @return string
+	 */
+	private function prepare_request_path( $path ) {
+		return apply_filters( 'sme_xmlrpc_path', $path );
+	}
+
+	/**
+	 * Set custom request headers.
+	 *
+	 * @param array $headers
+	 *
+	 * @return array
+	 */
+	private function prepare_request_headers( $headers ) {
+		return apply_filters( 'sme_client_headers', $headers );
+	}
+
+	/**
+	 * Handle failed request.
+	 *
+	 * @return array
+	 */
+	private function get_error_message() {
+
+		if ( strpos( $this->getErrorMessage(), 'requested method smeContentStaging.verify does not exist' ) !== false ) {
+			return $this->error_plugin_inactive();
+		}
+
+		if ( strpos( $this->getErrorMessage(), 'Could not resolve host' ) !== false ) {
+			return $this->error_host_not_found();
+		}
+
+		return $this->error_general();
+	}
+
+	/**
+	 * Content Staging plugin is not active.
+	 *
+	 * @return array
+	 */
+	private function error_plugin_inactive() {
+		$message = new Message();
+		$message->set_level( 'error' );
+		$message->set_message(
+			sprintf( 'Content Staging plugin not activated on host <strong>%s</strong>', $this->server )
+		);
+
+		return array(
+			'status'   => 2,
+			'messages' => array( $message ),
+		);
+	}
+
+	/**
+	 * Remote host could not be found.
+	 *
+	 * @return array
+	 */
+	private function error_host_not_found() {
+		$message = new Message();
+		$message->set_level( 'error' );
+		$message->set_message(
+			sprintf( 'Could not connect to host <strong>%s</strong>', $this->server )
+		);
+
+		return array(
+			'status'   => 2,
+			'messages' => array( $message ),
+		);
+	}
+
+	/**
+	 * Error occurred during request.
+	 *
+	 * @return array
+	 */
+	private function error_general() {
+		$message = new Message();
+		$message->set_level( 'error' );
+		$message->set_message(
+			sprintf(
+				'%s - on host: %s (error code %s)',
+				$this->getErrorMessage(),
+				$this->server,
+				$this->getErrorCode()
+			)
+		);
+
+		return array(
+			'status'   => 2,
+			'messages' => array( $message ),
+		);
+	}
+
+	/************************************************************************
+	 * Handle Request.
+	 *
+	 * Operations carried out on Production to handle an incoming XML-RPC
+	 * request from Content Stage.
+	 ************************************************************************/
 
 	/**
 	 * Receive the XML-RPC request, authenticate and return a response.
@@ -167,16 +351,6 @@ class Client extends WP_HTTP_IXR_Client {
 	}
 
 	/**
-	 * Generate an access token for request validation.
-	 *
-	 * @param string $data
-	 * @return string
-	 */
-	private function generate_access_token( $data ) {
-		return hash_hmac( 'sha1', $data, $this->secret_key );
-	}
-
-	/**
 	 * Prepare response data.
 	 *
 	 * @param array $response
@@ -187,21 +361,18 @@ class Client extends WP_HTTP_IXR_Client {
 		return $this->encode( serialize( $response ) );
 	}
 
+	/************************************************************************
+	 * Common
+	 ************************************************************************/
+
 	/**
-	 * @param string $method
-	 * @param array  $data
+	 * Generate an access token for request validation.
 	 *
-	 * @return array
+	 * @param string $data
+	 * @return string
 	 */
-	private function prepare_request_args( $method, $data = array() ) {
-
-		$data = $this->encode( serialize( $data ) );
-
-		return array(
-			$method,
-			$this->generate_access_token( $data ),
-			$data,
-		);
+	private function generate_access_token( $data ) {
+		return hash_hmac( 'sha1', $data, $this->secret_key );
 	}
 
 	/**
@@ -245,80 +416,4 @@ class Client extends WP_HTTP_IXR_Client {
 		}
 	}
 
-	/**
-	 * Handle failed request.
-	 *
-	 * @return array
-	 */
-	private function query_failed() {
-
-		if ( strpos( $this->getErrorMessage(), 'requested method smeContentStaging.verify does not exist' ) !== false ) {
-			return $this->error_plugin_inactive();
-		}
-
-		if ( strpos( $this->getErrorMessage(), 'Could not resolve host' ) !== false ) {
-			return $this->error_host_not_found();
-		}
-
-		return $this->error_general();
-	}
-
-	/**
-	 * Content Staging plugin is not active.
-	 *
-	 * @return array
-	 */
-	private function error_plugin_inactive() {
-		$message = new Message();
-		$message->set_level( 'error' );
-		$message->set_message(
-			sprintf( 'Content Staging plugin not activated on host <strong>%s</strong>', $this->server )
-		);
-
-		return array(
-			'status'   => 2,
-			'messages' => array( $message ),
-		);
-	}
-
-	/**
-	 * Remote host could not be found.
-	 *
-	 * @return array
-	 */
-	private function error_host_not_found() {
-		$message = new Message();
-		$message->set_level( 'error' );
-		$message->set_message(
-			sprintf( 'Could not connect to host <strong>%s</strong>', $this->server )
-		);
-
-		return array(
-			'status'   => 2,
-			'messages' => array( $message ),
-		);
-	}
-
-	/**
-	 * Error occurred during request.
-	 *
-	 * @return array
-	 */
-	private function error_general() {
-		$message = new Message();
-		$message->set_level( 'error' );
-		$message->set_message(
-			sprintf(
-				'%s - on host: %s (error code %s)',
-				$this->getErrorMessage(),
-				$this->server,
-				$this->getErrorCode()
-			)
-		);
-
-		return array(
-			'status'   => 2,
-			'messages' => array( $message ),
-		);
-	}
 }
