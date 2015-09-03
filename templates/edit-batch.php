bafd13a353f54c185ad410d88672e2676629de11
Marc Russell <marc@openboxstudios.com>
2015-09-03 15:29:17 -0400

    Update UI of buttons.
    Pre-Flight which creates an active deployment moved first and set to primary blue so user can deploy. Secondary aspect of creating a batch to deploy later moved to second position.

--- templates/edit-batch.php
+++ templates/edit-batch.php
@@ -17,16 +17,21 @@
 		<?php wp_nonce_field( 'sme-save-batch','sme_save_batch_nonce' ); ?>
 		<input type="hidden" name="post_ids" value="<?php echo $post_ids; ?>">
 
-		<input type="text" name="batch_title" size="30" value="<?php echo $batch->get_title(); ?>" class="sme-input-text" placeholder="Enter batch title here" autocomplete="off">
+		<input type="text" name="batch_title" size="30" value="<?php echo $batch->get_title(); ?>" class="sme-input-text" placeholder="Enter batch title here (optional)" autocomplete="off">
+
+		<?php submit_button( 'Pre-Flight Batch', 'primary', 'submit', false ); ?>
+		<?php submit_button( 'Save Batch', 'secondary', 'submit', false ); ?>
+		<input type="button" name="button" id="button" class="button" onclick="location.href='<?php echo admin_url( 'admin.php?page=sme-list-batches' ); ?>'" value="Cancel">
 
 		<?php echo $filters; ?>
 		<?php $table->display(); ?>
 
 		<?php do_action( 'sme_view_edit_batch_pre_buttons', $batch ); ?>
 
-		<?php submit_button( 'Save Batch', 'primary', 'submit', false ); ?>
-		<?php submit_button( 'Pre-Flight Batch', 'secondary', 'submit', false ); ?>
+		<?php submit_button( 'Pre-Flight Batch', 'primary', 'submit', false ); ?>
+		<?php submit_button( 'Save Batch', 'secondary', 'submit', false ); ?>
 		<input type="button" name="button" id="button" class="button" onclick="location.href='<?php echo admin_url( 'admin.php?page=sme-list-batches' ); ?>'" value="Cancel">
+	
 	</form>
 
 </div>
